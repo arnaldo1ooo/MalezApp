51edd38ea78b132fbe5f233069f0df191462d7ad
arnaldo1ooo <arnaldo1ooo@hotmail.com>
2020-08-16 10:44:10 -0400

    16-08-20

--- app/src/main/java/com/arnaldo/malezapp/areaimageclick/ActivityBuscarDistribucion.java
+++ app/src/main/java/com/arnaldo/malezapp/areaimageclick/ActivityBuscarDistribucion.java
@@ -4,15 +4,12 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TabHost;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +17,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.arnaldo.malezapp.R;
 import com.arnaldo.malezapp.conexion.Conexion;
 import com.arnaldo.malezapp.lista.ActivityLista;
-import com.arnaldo.malezapp.metodos.Metodos;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,13 +27,15 @@ public class ActivityBuscarDistribucion extends AppCompatActivity implements OnC
 
     private ImageView ivMapa;
     private ListView lvdpto;
-    private PhotoViewAttacher attacher;
-    private Metodos metodos = new Metodos();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscardistribucion);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Activar boton atras
+        getSupportActionBar().setDisplayShowHomeEnabled(true); //Activar icono en actionbar
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher_round); //Asignar icono
+
 
         ivMapa = findViewById(R.id.ivMapa);
         lvdpto = findViewById(R.id.lvDpto);
@@ -97,51 +95,50 @@ public class ActivityBuscarDistribucion extends AppCompatActivity implements OnC
         clickableAreasImage.setClickableAreas(clickableAreas);
     }
 
-    // Escucha los toques por la imagen y devuelve el dato de ese lugar
-    @Override
-    public void onClickableAreaTouched(Object item) {
-        if (item instanceof ItemClickDptos) { //Compara
-            String itemSelect = ((ItemClickDptos) item).getName();
-
-            Toast.makeText(this, metodos.SacarAcentos(itemSelect.toUpperCase()), Toast.LENGTH_SHORT).show();
-            System.out.println("itemSelect " + metodos.SacarAcentos(itemSelect.toUpperCase()));
-        }
-    }
-
     // Define your clickable area (pixel values: x coordinate, y coordinate, width, height) and assign an object to it
     @NonNull
     private List<ClickableArea> AreasClickables() {
         //Se elije un punto XY, a partir de ese punto se trasa una linea horizontal(w) y vertical(h) para formar un cuadrado clickable
         List<ClickableArea> areasClickables = new ArrayList<>();
-        areasClickables.add(new ClickableArea(67, 215, 306, 424, new ItemClickDptos("Boquerón")));
-        areasClickables.add(new ClickableArea(403, 64, 267, 372, new ItemClickDptos("Alto Paraguay")));
-        areasClickables.add(new ClickableArea(383, 554, 321, 283, new ItemClickDptos("Presidente Hayes")));
-        areasClickables.add(new ClickableArea(698, 461, 175, 180, new ItemClickDptos("Concepción")));
-        areasClickables.add(new ClickableArea(934, 470, 89, 210, new ItemClickDptos("Amambay")));
-        areasClickables.add(new ClickableArea(776, 652, 161, 211, new ItemClickDptos("San Pedro")));
-        areasClickables.add(new ClickableArea(949, 742, 247, 70, new ItemClickDptos("Canindeyú")));
-        areasClickables.add(new ClickableArea(772, 890, 95, 58, new ItemClickDptos("Cordillera")));
-        areasClickables.add(new ClickableArea(884, 881, 165, 75, new ItemClickDptos("Caaguazú")));
-        areasClickables.add(new ClickableArea(1060, 845, 116, 175, new ItemClickDptos("Alto Paraná")));
-        areasClickables.add(new ClickableArea(1046, 959, 94, 123, new ItemClickDptos("Alto Paraná")));
-        areasClickables.add(new ClickableArea(731, 917, 18, 118, new ItemClickDptos("Central")));
-        areasClickables.add(new ClickableArea(749, 995, 97, 115, new ItemClickDptos("Paraguarí")));
-        areasClickables.add(new ClickableArea(859, 994, 101, 44, new ItemClickDptos("Guairá")));
-        areasClickables.add(new ClickableArea(850, 1078, 166, 34, new ItemClickDptos("Caazapá")));
-        areasClickables.add(new ClickableArea(634, 1088, 70, 169, new ItemClickDptos("Ñeembucú")));
-        areasClickables.add(new ClickableArea(757, 1139, 100, 145, new ItemClickDptos("Misiones")));
-        areasClickables.add(new ClickableArea(865, 1168, 197, 73, new ItemClickDptos("Itapúa")));
+        areasClickables.add(new ClickableArea(67, 215, 306, 424, new ItemClickDptos("BOQUERÓN")));
+        areasClickables.add(new ClickableArea(403, 64, 267, 372, new ItemClickDptos("ALTO PARAGUAY")));
+        areasClickables.add(new ClickableArea(383, 554, 321, 283, new ItemClickDptos("PRESIDENTE HAYES")));
+        areasClickables.add(new ClickableArea(698, 461, 175, 180, new ItemClickDptos("CONCEPCIÓN")));
+        areasClickables.add(new ClickableArea(934, 470, 89, 210, new ItemClickDptos("AMAMBAY")));
+        areasClickables.add(new ClickableArea(776, 652, 161, 211, new ItemClickDptos("SAN PEDRO")));
+        areasClickables.add(new ClickableArea(949, 742, 247, 70, new ItemClickDptos("CANINDEYÚ")));
+        areasClickables.add(new ClickableArea(772, 890, 95, 58, new ItemClickDptos("CORDILLERA")));
+        areasClickables.add(new ClickableArea(884, 881, 165, 75, new ItemClickDptos("CAAGUAZÚ")));
+        areasClickables.add(new ClickableArea(1060, 845, 116, 175, new ItemClickDptos("ALTO PARANÁ")));
+        areasClickables.add(new ClickableArea(1046, 959, 94, 123, new ItemClickDptos("ALTO PARANÁ")));
+        areasClickables.add(new ClickableArea(731, 917, 18, 118, new ItemClickDptos("CENTRAL")));
+        areasClickables.add(new ClickableArea(749, 995, 97, 115, new ItemClickDptos("PARAGUARÍ")));
+        areasClickables.add(new ClickableArea(859, 994, 101, 44, new ItemClickDptos("GUAIRÁ")));
+        areasClickables.add(new ClickableArea(850, 1078, 166, 34, new ItemClickDptos("CAAZAPÁ")));
+        areasClickables.add(new ClickableArea(634, 1088, 70, 169, new ItemClickDptos("ÑEEMBUCÚ")));
+        areasClickables.add(new ClickableArea(757, 1139, 100, 145, new ItemClickDptos("MISIONES")));
+        areasClickables.add(new ClickableArea(865, 1168, 197, 73, new ItemClickDptos("ITAPÚA")));
 
         return areasClickables;
     }
 
+    // Escucha los toques por la imagen y devuelve el dato de ese lugar
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
+    public void onClickableAreaTouched(Object item) {
+        if (item instanceof ItemClickDptos) { //Compara
+            String itemSelect = ((ItemClickDptos) item).getName();
+            Intent intent = new Intent(getApplicationContext(), ActivityLista.class);
+            intent.putExtra("btnSeleccionado", "distribucion");
+            intent.putExtra("dptoseleccionado", itemSelect);
+            startActivityForResult(intent, 0);
+            //Toast.makeText(this, metodos.SacarAcentos(itemSelect.toUpperCase()), Toast.LENGTH_SHORT).show();
+        }
     }
 
+    //Para que al oprimir en boton subir, retroceda al ultimo activity
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return false;
     }
 }
